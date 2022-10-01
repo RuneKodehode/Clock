c92a57b37df8e0194dcdb87b9ecc9ed0cb6cb620
RuneKodehode <112623367+RuneKodehode@users.noreply.github.com>
2022-10-01 03:11:02 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
 let slider = document.getElementById("sliderOne")
 let slider2 = document.getElementById("sliderTwo")
 let slider3 = document.getElementById("sliderThree")
-let bodyy = document.getElementsByName("body")
 scale = 0;
 scale2 = 0;
+let timer = document.getElementById("timer");
+let Array = [1];
+
+// få visarane te å spinna i klokko samt sekundtelljar
 function btn() {
 if (!scale) {
+    tids = setInterval (() => {
+        timer.textContent = Array[0]++ + "0" +"ms";
+    }, 10);
     slider.style.animationPlayState = "running"
     slider2.style.animationPlayState = "running"
     slider3.style.animationPlayState = "running"
     scale = 1
+ return
 } else {
+   clearInterval(tids)
+        timer.textContent = 0;
     slider.style.animationPlayState = "paused"
     slider2.style.animationPlayState = "paused"
     slider3.style.animationPlayState = "paused"
@@ -19,15 +28,29 @@ if (!scale) {
 }
 }
 
+// få bodyen te å spinna i sin egen axis
+let bodyy = document.getElementsByName("body")
 function btn2() {
     if (!scale2) {
        document.body.style.animationPlayState = "running"
-       console.log("wtf")
         scale2 = 1
     } else {
         document.body.style.animationPlayState = "paused"
-       
         scale2 = 0;
     }
     }
    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
